dc0d1d4fdadd5b6150cc10bdd4f1de8d17c416fa
Luca Marcelli <luca@marcelli.ch>
2024-08-13 15:48:50 +0200

    done

--- src/main.c
+++ src/main.c
@@ -1,14 +1,19 @@
-// =============================================================================
-// File inclusion
-// =============================================================================
 #include <stdbool.h>
 #include <stddef.h>
 
 #include "common.h"
 
-// =============================================================================
-// Public functions definitions
-// =============================================================================
+extern void write_to_fiq_banks();
+extern void write_to_svc_banks();
+extern void write_to_abt_banks();
+extern void write_to_irq_banks();
+extern void load_all();
+
 void main(void) {
+    write_to_fiq_banks();
+    write_to_svc_banks();
+    write_to_abt_banks();
+    write_to_irq_banks();
+    load_all();
     while(true);
 }
